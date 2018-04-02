c91a4c1eb8abf1e6bd9bfec3a6737d6437c29fb0
jasonLiu001 <wangliu@isoftstone.com>
2018-04-02 14:24:01 +0800

    updated.

--- shell/deploy_site.sh
+++ shell/deploy_site.sh
@@ -2,3 +2,16 @@
 
 # 编译站点
 mvn clean package;
+
+# 拷贝jar包
+cp -rf /home/github/spring-boot-blog/target/blog-1.0.3.jar /root
+
+# 拷贝sh脚本
+cp -rf /home/github/spring-boot-blog/doc/script/startup.sh /root
+cp -rf /home/github/spring-boot-blog/doc/script/shutdown.sh /root
+
+# 停止站点
+sh /root/shutdown.sh
+
+# 启动站点
+sh /root/startup.sh
