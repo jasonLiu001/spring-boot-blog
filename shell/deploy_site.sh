b1d9c606c75c5accb3464e389b1c3b1dac0cdd11
jasonLiu001 <wangliu@isoftstone.com>
2018-04-02 14:36:32 +0800

    添加部署文件

--- shell/deploy_site.sh
+++ shell/deploy_site.sh
@@ -4,14 +4,14 @@
 mvn clean package;
 
 # 拷贝jar包
-cp -rf /home/github/spring-boot-blog/target/blog-1.0.3.jar /root
+cp -rf /home/github/spring-boot-blog/target/blog-1.0.3.jar /root/bysite
 
 # 拷贝sh脚本
-cp -rf /home/github/spring-boot-blog/doc/script/startup.sh /root
-cp -rf /home/github/spring-boot-blog/doc/script/shutdown.sh /root
+cp -rf /home/github/spring-boot-blog/doc/script/startup.sh /root/bysite
+cp -rf /home/github/spring-boot-blog/doc/script/shutdown.sh /root/bysite
 
 # 停止站点
-sh /root/shutdown.sh
+sh /root/bysite/shutdown.sh
 
 # 启动站点
-sh /root/startup.sh
+sh /root/bysite/startup.sh
