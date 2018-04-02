8767106d47c2cdeb69c84572b9f2d8439d9d62fb
jasonLiu001 <wangliu@isoftstone.com>
2018-04-02 14:38:32 +0800

    更新脚本

--- shell/deploy_site.sh
+++ shell/deploy_site.sh
@@ -6,10 +6,6 @@ mvn clean package;
 # 拷贝jar包
 cp -rf /home/github/spring-boot-blog/target/blog-1.0.3.jar /root/bysite
 
-# 拷贝sh脚本
-cp -rf /home/github/spring-boot-blog/doc/script/startup.sh /root/bysite
-cp -rf /home/github/spring-boot-blog/doc/script/shutdown.sh /root/bysite
-
 # 停止站点
 sh /root/bysite/shutdown.sh
 
