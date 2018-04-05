ca561f07937ce515cd2c7ffcdab0fe4a28ea1386
jasonLiu001 <wangliu@isoftstone.com>
2018-04-05 12:00:25 +0800

    added new class

---
+++ src/main/java/cn/eyeblue/blog/rest/search/Search.java
@@ -0,0 +1,16 @@
+package cn.eyeblue.blog.rest.search;
+
+import cn.eyeblue.blog.rest.base.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@Entity
+public class Search extends BaseEntity {
+
+}
